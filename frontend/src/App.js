c6ade0a2df46dcbd38792f399b4c89e31a305645
Vivek Malviya <vivek.malviya@valtech.com>
2022-06-15 20:16:01 +0200

    Added Image API and Delete functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Search from './components/Search'
+import ImageCard from './components/ImageCard'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 const App = () => {
   const [word, setWord] = useState('')
+  const [images, setImages] = useState([])
 
   const handleSearchSubmit = (e) => {
     e.preventDefault()
@@ -16,18 +19,31 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        setImages([{ ...data, title: word }, ...images])
       })
       .catch((error) => {
         console.log(error)
       })
     setWord('')
   }
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id))
+  }
+
   return (
     <div className="App">
       <Header title="Image Gallery" />
-
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-4">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   )
 }
