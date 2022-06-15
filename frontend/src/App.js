9b35a8f40fbacdd07061f0825c488d159a5e388f
Vivek Malviya <vivek.malviya@valtech.com>
2022-06-15 18:58:25 +0200

    Added some linting configs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
-function App() {
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
+import Search from './components/Search'
+
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+
+const App = () => {
+  const [word, setWord] = useState('')
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+
+    fetch(
+      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    setWord('')
+  }
   return (
-    <div className='App'>
-      <h1>Images Gallery</h1>
+    <div className="App">
+      <Header title="Image Gallery" />
+
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   )
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Navbar, Container } from 'react-bootstrap'
+
+const navbarStyle = {
+  backgroundColor: 'lightblue',
+}
+const Header = (props) => {
+  const { title } = props
+  return (
+    <Navbar style={navbarStyle} variant='light'>
+      <Container>
+        <Navbar.Brand href='/'>{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
